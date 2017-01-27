e7ad873b6f01c2bba167f30b6183421a5d3c3b34
charlymessin <charlymessin@172.20.10.4>
2017-01-27 11:54:21 +0100

    premier push de l'admin des élèves

---
+++ adminEleve.php
@@ -0,0 +1,15 @@
+<php
+	<head>
+		<link href="css.bootstrap.min.css" rel="stylesheet">
+	</head>
+			<body>
+				<form>
+					<diffv class="row">
+						<div class="col-md-2"></div>
+							<div class="col-md-4">
+								<center><h1>Administration des eleves</H1></center>
+							</div>
+						<div class="col-md-2"></div>					
+					</div>
+				</form>
+			</body>
