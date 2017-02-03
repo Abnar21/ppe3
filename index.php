2ebed7bb3ce5f7fb661fc65c0fcbd0803d2d84e5
Loxxo <Loxxo@LAPTOP-83FO1JJG>
2017-02-03 10:23:43 +0100

    TEst ajout sur index burisk

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1"> 
 <meta name="description" content=""> 
 <meta name="author" content=""> 
-<title>Accueil</title> 
+<title>Accueil burisk</title> 
 
 <link href="css/bootstrap.min.css" rel="stylesheet"> 
 
