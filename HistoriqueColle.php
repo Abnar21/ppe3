cd632f8410563ef5c93fb3087694f568e7592b79
Elodie <Elodie@PC-ELODIE>
2017-02-03 10:01:23 +0100

    historique des colles

---
+++ HistoriqueColle.php
@@ -0,0 +1,26 @@
+<div class="row">
+    <div class="col-md-2"></div>
+        <div class="col-md-8">
+		
+		<div class="row">
+			<h1>Historique des Colles</h1>
+		</div>
+		
+		</div>    
+	<div class="col-md-2"></div>
+</div>
+	<div class="row">
+		Sélectionner une classe dans la liste suivante :
+	</div> 
+<select class="form-control">
+  <option>Classe1</option>
+  <option>Classe2</option>
+  <option>Classe3</option>
+  <option>Classe4</option>
+  <option>Classe5</option>
+</select>
+<div class="row">
+	<table class="table table-bordered">
+	</table>
+</div>
+
