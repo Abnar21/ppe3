19ac4fb0243dc84bb3cbd2908ca66f9604f77c4f
charlymessin <charlymessin@172.20.10.4>
2017-02-17 10:38:11 +0100

    accueil : j'ai mis, au lieu de "laizbzeilbfai", l'utilité du site. adminEleve : ajout de la listebox + boutton validé, il manque les href

--- views/eleve/adminEleve.php
+++ views/eleve/adminEleve.php
@@ -1,1 +1,19 @@
-<center><h1> Administration des élèves </h1></center>
+<head>
+    
+</head>
+
+<body>
+    <header class="business-header">
+        <div class="container"> </body>
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="tagline">Administration des élèves</h1>
+                </div>
+            </div>
+    </header>
+    
+    <h4> selectionner la classe de l'élève : </h4>
+<select class="form-control">
+    <?php foreach ($classe as $uneClasse) ?>
+</select>
+    <h4><br><center><button><a href="adminEleve.php?action=uneClasse"/>Valider</button></center></h4>
