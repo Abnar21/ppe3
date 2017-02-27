0f7f4d11088842f9a34a852ef37ec890d5264684
Quentin <Quentin@Abnar21>
2017-02-27 16:57:30 +0100

    Modif functions.php

--- PDO/functions.php
+++ PDO/functions.php
@@ -19,5 +19,20 @@ function getAllClass(){
         echo 'Échec lors de la récupération des sujets : ' . $e->getMessage();
     }
 }
+function admin($login,$pwd){
+   try{
+        $connection = new PDO("mysql:host=localhost;dbname=".BD.";charset=utf8", USER_BD, PWD_BD);
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $requete=$connection->prepare('SELECT COUNT(*)as nb FROM prof WHERE login=:login AND pwd=:pwd');
+        $requete->bindValue(':login',$login, PDO::PARAM_STR);
+        $requete->bindValue(':pwd', $pwd, PDO::PARAM_STR);
+        $requete->execute();
+        $isAdmin=1;
+        $isAdmin=$requete->fetch();;
+        $connection=null;
+    } catch (PDOException $e) {
+        echo ('Echec lors de la connexion : user ou mdp incorrect') . $e->getMessage();
+    }
+}
 ?>
 
